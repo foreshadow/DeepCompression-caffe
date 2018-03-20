ecad7754f9357cd10bea56af2fb1bf7f63e83ccb
Infinity <573679956@qq.com>
2018-03-20 01:28:04 -0700

    Update gitignore

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,2 +1,4 @@
 sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler libhdf5-dev -y
 sudo apt-get install --no-install-recommends libboost-all-dev -y
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev -y
+sudo apt-get install libatlas-base-dev -y
