12bf7ecf6e77272581a48f19f561eab684f1f50e
Infinity <573679956@qq.com>
2018-03-20 00:39:22 -0700

    Update scripts

---
+++ install-requirements.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler libhdf5-dev -y
+sudo apt-get install --no-install-recommends libboost-all-dev -y
